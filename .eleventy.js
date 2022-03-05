d4653b4b0ddbb78db91df1d0f9a9f6f6fc5b33e8
Julia Le <44715708+thuycis2018@users.noreply.github.com>
2022-03-05 00:30:25 -0800

    slowly upload files

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  const { DateTime } = require("luxon");
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
