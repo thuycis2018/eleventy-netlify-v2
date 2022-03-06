be8f6ae5eba6a4a0d538f3b0a4f96e44db67397f
Julia Le <44715708+thuycis2018@users.noreply.github.com>
2022-03-05 21:03:25 -0800

    added ico

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/admin");
   eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   eleventyConfig.addPassthroughCopy("./src/assets");
 
   const { DateTime } = require("luxon");
